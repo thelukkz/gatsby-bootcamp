ac5bc38083518c1cad6e89fa947191addeda8250
Łukasz Bednarek <bednarek.lukasz@gmail.com>
2019-05-11 11:25:55 +0200

    add gatsby-source-filesystem plugin
    with configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,13 @@ module.exports = {
         author: 'Łukasz Bednarek'
     },
     plugins: [
-        'gatsby-plugin-sass'
+        'gatsby-plugin-sass',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'src',
+                path: `${__dirname}/src/`
+            }
+        }
     ]
 }
