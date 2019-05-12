78a49bb798d7ecaae38f7dc752bf2d259f6d9675
Łukasz Bednarek <bednarek.lukasz@gmail.com>
2019-05-12 18:26:17 +0200

    add styles
    to the list of posts/ footer/ blog post files reordering

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link, graphql, useStaticQuery } from 'gatsby'
 
 import Layout from '../components/layout'
+import blogStyles from './blog.module.scss'
 
 const BlogPage = () => {
     const data = useStaticQuery(graphql`
@@ -26,16 +27,14 @@ const BlogPage = () => {
     return (
         <Layout>
             <h1>Blog</h1>
-            <ol>
+            <ol className={blogStyles.posts}>
                 {data.allMarkdownRemark.edges.map((edge) => {
                     return (
-                        <li key={edge.node.fields.slug}>
-                            <h2>
-                                <Link to={`/blog/${edge.node.fields.slug}`}>
-                                    {edge.node.frontmatter.title}
-                                </Link>
-                            </h2>
-                            <p>{edge.node.frontmatter.date}</p>
+                        <li key={edge.node.fields.slug} className={blogStyles.post}>
+                            <Link to={`/blog/${edge.node.fields.slug}`}>
+                                    <h2>{edge.node.frontmatter.title}</h2>
+                                    <p>{edge.node.frontmatter.date}</p>
+                            </Link>                          
                         </li>
                     )
                 })}
